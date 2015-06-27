97fe47f772bf0d1148558f40cebc39ca0afc6194
Matt <matthewmorgante@gmail.com>
2015-06-27 09:31:56 -0500

    Workouts#index/new

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,6 +1,8 @@
 class WorkoutsController < ApplicationController
+  before_action :find_workout, only: [:show, :edit, :update, :destroy]
+
   def index
-     
+    @workouts = Workout.all.order("created_at DESC")
   end 
 
   def create 
@@ -17,7 +19,6 @@ class WorkoutsController < ApplicationController
   end 
 
   def show 
-
   end 
 
   def edit 
@@ -39,6 +40,6 @@ class WorkoutsController < ApplicationController
   end
 
   def find_workout
-    
+    @workout = Workout.find(params[:id])
   end
 end
